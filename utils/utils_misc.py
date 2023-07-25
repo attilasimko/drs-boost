f1ccabc347d6db0993a31c51d227b001f1f4a6be
Attila <attila.simko@umu.se>
2023-07-25 10:10:17 +0200

    batch size prune for Gerd

--- utils/utils_misc.py
+++ utils/utils_misc.py
@@ -263,6 +263,8 @@ def prune_config(config, name):
     if (name == "gerd"):
         config["parameters"]["num_filters"] = 24
         config["parameters"]["optimizer"] = "Adam"
-        print("Parameters pruned: num_filters = 24, optimizer = Adam")
+        config["parameters"]["batch_size"] = 4
+        print("Parameters pruned: num_filters = 24, optimizer = Adam, batch_size = 4")
+
     
     return config
