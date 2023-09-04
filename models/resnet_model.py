85f336586d15102e95860c8666f6d74a54b607f5
Attila <attila.simko@umu.se>
2023-09-04 09:10:47 +0200

    sigmoid again for resnet

--- models/resnet_model.py
+++ models/resnet_model.py
@@ -127,7 +127,7 @@ class ResNetModel(BaseModel):
         x = Dropout(dropout_rate)(x)
         x = Dense(num_filters, activation='relu')(x)
         x = Dropout(dropout_rate)(x)
-        x = Dense(len(generator.outputs), activation="softmax")(x)
+        x = Dense(len(generator.outputs), activation="sigmoid")(x)
 
         outputs = []
         for i in range(len(generator.outputs)):
